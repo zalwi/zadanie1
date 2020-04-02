c07c42faafcc0f826adaeafbf5006acac3b82cd0
Kamil Zalwert <kamilz89@gmail.com>
2020-04-02 14:44:36 +0200

    Drugi commit - dodano dwie linie w wyniku

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ class HelloWorld {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }

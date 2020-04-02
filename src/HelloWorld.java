92fe3ba1a4271f708ecd26d283a5d4bb70a17caf
Kamil Zalwert <kamilz89@gmail.com>
2020-04-02 14:57:42 +0200

    Powtarzam całą operację, dodałem komentarze

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
 class HelloWorld {
     public static void main(String[] args) {
+        /*  testuje
+            blok
+            komentarza */
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        // Poniżej dodałem zmiany do pierwotnej wersji
         System.out.println("i");
         System.out.println("commitowania");
     }

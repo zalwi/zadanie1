fef9dcdf82a2ba7b6fecf544d73e3c1fae2b6b6b
Kamil Zalwert <kamilz89@gmail.com>
2020-04-02 14:40:25 +0200

    Pierwszy commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+}
